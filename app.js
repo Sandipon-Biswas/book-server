1d28095323ed9c3f0e3f30c830898293ddc582dd
sandipon biswas <98538000+Sandipon-Biswas@users.noreply.github.com>
2023-03-26 01:54:23 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const express =require('express');
+const cors =require('cors');
+require('./config/db')
+ const userRouter =require('./routers/book.route');
+const app =express();
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(cors());
+app.use('/books', userRouter )
+app.get('/',(req,res)=>{
+    res.send("hello world")
+});
+//sdfsdfsdf
+
+app.use((req,res,next)=>{
+    res.status(404).json({
+        "message":"route not found"
+    })
+});
+
+app.use((err,req,res,next)=>{
+    res.status(500).json({
+        "message":"something broke"
+    })
+});
+
+
+module.exports=app;
